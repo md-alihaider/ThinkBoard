37b0614ed598d11065bfb28f67d6024700d5f24b
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-28 11:52:01 +0530

    feat: restructure backend with new server and routes for notes management

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  createNote,
+  deleteNoteById,
+  getAllNotes,
+  getNoteById,
+  updateNoteById,
+} from "../controllers/notesController.js";
+
+const router = express.Router();
+
+//read
+router.get("/", getAllNotes);
+
+//create
+router.post("/", createNote);
+
+//read by id
+router.get("/:id", getNoteById);
+
+//delete
+router.delete("/:id", deleteNoteById);
+
+//update by id
+router.put("/:id", updateNoteById);
+
+export default router;
