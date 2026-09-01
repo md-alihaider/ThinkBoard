8f28c11814461357a6b4adb55fccc512e47acbfa
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-09-01 20:09:09 +0530

    feat: implement rate limiting with Upstash and update server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,19 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-connectDB();
 
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+  });
 });
