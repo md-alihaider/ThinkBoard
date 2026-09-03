6fad56b1087fd730bcbff177a8cc85547cb434b3
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-09-03 19:08:21 +0530

    feat: add CORS support, implement note fetching with rate limiting, and enhance UI components

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,17 @@ import notesRoutes from "./routes/notesRoutes.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 app.use(rateLimiter);
 
